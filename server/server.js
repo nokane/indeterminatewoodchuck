11077d0e5c547362c4c47bf38270384a4266020a
John Paulino <paulinoj@gmail.com>
2015-07-13 16:34:15 -0700

    (feat/server-socket) fixed typo in require

--- server/server.js
+++ server/server.js
@@ -3,14 +3,14 @@ var bodyParser = require('body-parser');
 
 var app = express();
 var server = require('http').Server(app);
-var io = requre('socket.io')(server);
+var io = require('socket.io')(server);
 
 // app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/../client/dist'));
 
-var socketroute = require('socketroute.js');
+var socketroute = require('./socketroute.js');
 
 io.on('connection', function(socket) {
   socketroute(io, socket);
