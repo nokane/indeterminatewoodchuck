4b7908dfa0244c94d76cdc05c87a948fe3a891b4
John Paulino <paulinoj@gmail.com>
2015-07-13 15:58:02 -0700

    (feat/socket-server) add socket.io and socketroute modules

--- server/server.js
+++ server/server.js
@@ -2,10 +2,18 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var app = express();
+var server = require('http').Server(app);
+var io = requre('socket.io')(server);
 
 // app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/../client/dist'));
+
+var socketroute = require('socketroute.js');
+
+io.on('connection', function(socket) {
+  socketroute(io, socket);
+});
 
 module.exports = app;
