f67b29c68565406f089055854709be67953608ac
John Paulino <paulinoj@gmail.com>
2015-07-13 20:47:24 -0700

    (feat/server-socket) attach socketroute object to app

--- server/server.js
+++ server/server.js
@@ -2,7 +2,9 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var app = express();
+
 var server = require('http').Server(app);
+server.listen(3000);
 var io = require('socket.io')(server);
 
 // app.use(bodyParser.urlencoded({extended: true}));
@@ -10,10 +12,10 @@ app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/../client/dist'));
 
-var socketroute = require('./socketroute.js');
+app.socketroute = require('./socketroute.js');
 
 io.on('connection', function(socket) {
-  socketroute(io, socket);
+  app.socketroute.socketroute(io, socket);
 });
 
 module.exports = app;
