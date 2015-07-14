160b3620e4f0f3416641f2105dc86fc0b8be647d
Niall O'Kane <okanenm@gmail.com>
2015-07-14 12:03:04 -0700

    (feat/server-socket) add second test to check that new roomname is created for 2nd employee

--- spec/server/socket.js
+++ spec/server/socket.js
@@ -1,7 +1,6 @@
 var expect = require('chai').expect;
 var io = require('socket.io-client');
 var ioserver = require('socket.io');
-var socketRoute = require('./../../server/socketroute.js');
 var socketTestURL = 'http://127.0.0.1:3000';
 
 var options = {
@@ -9,7 +8,7 @@ var options = {
   'force new connection': true
 };
 
-var server = require('../../server/server.js');
+var server = require('../../index.js');
 describe('Socket.io Server Routing', function() {
   it('Should create new room on "staffReady"', function(done) {
     var staffSocket = io.connect(socketTestURL, options);
@@ -17,8 +16,20 @@ describe('Socket.io Server Routing', function() {
       staffSocket.emit('staffReady', 'roomname1')
     });
     staffSocket.on('staffRoom', function(name) {
-      var rooms = server.socketroute.rooms;
-      expect(rooms).to.have.length(1);
+      expect(name).to.equal('room1');
+      staffSocket.disconnect();
+      done();
+    });
+  });
+
+  it('Second roomname should equal "room2"', function(done) {
+    var staffSocket1 = io.connect(socketTestURL, options);
+    staffSocket1.on('connect', function(data){
+      staffSocket1.emit('staffReady', 'roomname1')
+    });
+    staffSocket1.on('staffRoom', function(name) {
+      expect(name).to.equal('room2');
+      staffSocket1.disconnect();
       done();
     });
   });
