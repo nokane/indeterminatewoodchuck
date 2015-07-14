42c4f807ef683be2917efab3b0b37099721f530b
Niall O'Kane <okanenm@gmail.com>
2015-07-14 14:31:16 -0700

    (feat/server-socket) implement listener for user on "customerRequest"

--- server/socketroute.js
+++ server/socketroute.js
@@ -2,6 +2,8 @@ var socket = module.exports = {};
 
 socket.rooms = [];
 socket.num = 0;
+socket.customerNum = 0;
+socket.customerQueue = [];
 
 socket.socketroute = function(io, user) {
   user.on('staffReady', function() {
@@ -9,5 +11,15 @@ socket.socketroute = function(io, user) {
     var roomname = "room" + socket.num;
     socket.rooms.push(roomname);
     io.to(user.id).emit('staffRoom', roomname);
+  });
+
+  user.on('customerRequest', function() {
+    var customerName = 'customer' + socket.customerNum;
+    socket.customerNum += 1;
+    socket.customerQueue.push(customerName);
+    if (socket.rooms.length !== 0) {
+      io.to(user.id).emit('customerRoom', socket.rooms.shift());
+      socket.customerQueue.shift();
+    }
   });
 };
