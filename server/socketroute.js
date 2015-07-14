0d8e0732282194c64e40cafa685780b2f1bea3ce
Niall O'Kane <okanenm@gmail.com>
2015-07-14 16:36:56 -0700

    (feat/server-socket) remove roomname of disconnected staff from socket.rooms

--- server/socketroute.js
+++ server/socketroute.js
@@ -3,12 +3,16 @@ var socket = module.exports = {};
 socket.rooms = [];
 socket.num = 0;
 socket.customerQueue = [];
+socket.staff = {};
 
 socket.socketroute = function(io, user) {
+  user.category = undefined;
   user.on('staffReady', function() {
+    user.category = "staff";
     socket.num += 1;
     var roomname = "room" + socket.num;
     socket.rooms.push(roomname);
+    socket.staff[user.id] = roomname;
     io.to(user.id).emit('staffRoom', roomname);
     if (socket.customerQueue.length > 0) {
       var customerId = socket.customerQueue.shift();
@@ -22,5 +26,14 @@ socket.socketroute = function(io, user) {
       io.to(user.id).emit('customerRoom', socket.rooms.shift());
       socket.customerQueue.shift();
     }
+  });
+
+  user.on('disconnect', function() {
+    if (user.category === "staff") {
+      var roomIndex = socket.rooms.indexOf(socket.staff[user.id])
+      if (roomIndex != -1) {
+        socket.rooms.splice(roomIndex, 1);
+      }
+    } 
   });
 };
