6911212bf5b577e2b875ddebe87c2606b4d8fafa
John Paulino <paulinoj@gmail.com>
2015-07-13 20:57:48 -0700

    (feat/server-socket) test room is created on staffReady

--- spec/server/socket.js
+++ spec/server/socket.js
@@ -1,23 +1,26 @@
-var should = require('should');
+var expect = require('chai').expect;
 var io = require('socket.io-client');
-var socketRoute = require('./../../server/socketroute.js')
-var socketTestURL = 'http://0.0.0.0:8080';
+var ioserver = require('socket.io');
+var socketRoute = require('./../../server/socketroute.js');
+var socketTestURL = 'http://127.0.0.1:3000';
 
 var options = {
   transports: ['websocket'],
   'force new connection': true
 };
 
-
+var server = require('../../server/server.js');
 describe('Socket.io Server Routing', function() {
   it('Should create new room on "staffReady"', function(done) {
     var staffSocket = io.connect(socketTestURL, options);
     staffSocket.on('connect', function(data){
-      staffSocket.emit('staffReady', 'roomname')
+      staffSocket.emit('staffReady', 'roomname1')
     });
-    socketRoute.rooms.length.should.equal(1);
-    done();
-
+    staffSocket.on('staffRoom', function(name) {
+      var rooms = server.socketroute.rooms;
+      expect(rooms).to.have.length(1);
+      done();
+    });
   });
 
 });
