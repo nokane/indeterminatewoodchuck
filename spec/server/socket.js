bbea9b2d9af5df531cbd8ccc67edea48e0fdb8ad
Niall O'Kane <okanenm@gmail.com>
2015-07-13 17:07:44 -0700

    (test/server-socket) initialize "Socket.io Server Routing" test

---
+++ spec/server/socket.js
@@ -0,0 +1,14 @@
+var should = require('should');
+var io = require('socket.io-client');
+
+var socketTestURL = 'http://0.0.0.0:8080';
+
+var options = {
+  transports: ['websocket'],
+  'force new connection': true
+};
+
+describe('Socket.io Server Routing', function() {
+
+
+});
