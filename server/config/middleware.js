fe4df0de9216e98664fca001b3764405a4075108
Leon Yip <lyip1992@gmail.com>
2015-07-17 12:10:58 -0700

    (feat) use checkAuth protection on '/' path

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,8 +1,9 @@
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
+var helpers = require('./helpers.js');
 
 module.exports = function(app, express, server, io){
-  // app.set('secret', 'disdasecretyo');
+  app.set('secret', 'disdasecretyo');
 
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
@@ -10,8 +11,12 @@ module.exports = function(app, express, server, io){
 
   app.use(express.static(__dirname + '/../../client/dist'));
   app.use('/library', express.static(__dirname + '/../../library'));
+  // app.get('/login', express.static(__dirname + '/../../login')); // don't protect /login route
 
   // protect the root route except for library and login/signup
+  // app.use(helpers.checkAuth);
+  // app.use(helpers.errorLogger);
+  // app.use(helpers.errorHandler);
 
   server.socketroute = require('./socketroute.js');
   io.on('connection', function(socket) {
