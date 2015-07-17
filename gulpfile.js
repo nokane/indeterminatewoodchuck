b0a16261f04a850d4ead1960b485ec1443689a9e
Niall O'Kane <okanenm@gmail.com>
2015-07-16 17:00:12 -0700

    (feat/db) implement 'shell-db-create' task to create dev environment Postgres DB

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,8 @@ var reactify = require('reactify');
 var streamify = require('gulp-streamify');
 var file = require('gulp-file');
 var argv = require('yargs').argv;
+var shell = require('gulp-shell');
+
 
 var path = {
   HTML: 'client/index.html',
@@ -57,6 +59,11 @@ gulp.task('test', function(){
             process.exit();
           });
 });
+
+gulp.task('shell-db-create', shell.task([
+  'psql postgres -c "CREATE DATABASE dev_supportal"',
+  'psql postgres -c "CREATE DATABASE test_supportal"'
+]));
 
 gulp.task('write-personal-config', function() {
   var user = argv.user;
