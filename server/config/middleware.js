49d0302b44b8c492bbd9cdf33bc0fb5f1837fe83
Leon Yip <lyip1992@gmail.com>
2015-07-17 11:39:11 -0700

    (refactor) eliminate separate protected route for users

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -2,7 +2,6 @@ var morgan = require('morgan');
 var bodyParser = require('body-parser');
 
 module.exports = function(app, express, server, io){
-  // var userRouter = express.Router();
   // app.set('secret', 'disdasecretyo');
 
   app.use(bodyParser.urlencoded({extended: true}));
@@ -12,8 +11,7 @@ module.exports = function(app, express, server, io){
   app.use(express.static(__dirname + '/../../client/dist'));
   app.use('/library', express.static(__dirname + '/../../library'));
 
-  // app.use('/api/users', userRouter);
-  // require('../users/userRoutes.js')(userRouter);
+  // protect the root route except for library and login/signup
 
   server.socketroute = require('./socketroute.js');
   io.on('connection', function(socket) {
