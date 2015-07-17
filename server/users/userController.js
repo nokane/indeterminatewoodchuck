2515c7799ba6b0e11e663c45165dfb7f59866db1
Leon Yip <lyip1992@gmail.com>
2015-07-17 12:12:19 -0700

    (refactor) remove checkAuth method

--- server/users/userController.js
+++ server/users/userController.js
@@ -31,25 +31,5 @@ module.exports = {
     // if user is in the database
       // call next because the user is already in the database
       // else create the new user and issue a new token
-  },
-
-  checkAuth: function(req, res, next){
-    var token = req.body.token || req.query.token || req.headers['x-access-token'];
-
-    if( token ){
-      jwt.verify(token, app.get('secret'), function(err, decoded){
-        if( err ){
-          return res.json({ success: false, message: 'Failed to authenticate token.' });
-        } else {
-          req.decoded = decoded; // if everything is good, then save to request for use in other routes
-          next();
-        }
-      });
-    } else { // if there is no token then return an error
-      return res.status(403).send({
-        success: false,
-        message: 'No token provided'
-      });
-    }
   }
 };
