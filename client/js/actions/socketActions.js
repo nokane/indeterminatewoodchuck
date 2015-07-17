f7ee0fb1ea83535c88cc7677a6198ef848bf9183
brettkan <bkfighton@gmail.com>
2015-07-17 10:12:57 -0700

    (feat) add socketActions and create JOIN_ROOM constant to emit to dispatcher

---
+++ client/js/actions/socketActions.js
@@ -0,0 +1,14 @@
+var AppDispatcher = require('../dispatchers/appDispatcher');
+var appConstants = require('../constants/appConstants');
+
+var socket = io();
+
+socket.on('staffRoom', function(data) {
+  console.log('Staff connecting to room with name: ', data);
+  AppDispatcher.dispatch({
+    type: appConstants.JOIN_ROOM,
+    data: data
+  });
+});
+
+
--- client/js/constants/appConstants.js
+++ client/js/constants/appConstants.js
@@ -1,5 +1,6 @@
 var appConstants = {
-  START_SOCKET_LISTENER: "START_SOCKET_LISTENER"
+  START_SOCKET_LISTENER: "START_SOCKET_LISTENER",
+  JOIN_ROOM: "JOIN_ROOM"
 };
 
 module.exports = appConstants;
