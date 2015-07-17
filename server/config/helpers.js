8778fad0ada89d85ea202eb28a72ce088ea1f33a
Leon Yip <lyip1992@gmail.com>
2015-07-17 11:56:27 -0700

    (feat) fill checkAuth method in helpers.js

--- server/config/helpers.js
+++ server/config/helpers.js
@@ -1,7 +1,23 @@
 module.exports = {
 
   checkAuth: function(req, res, next){
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
 
+    if( token ){
+      jwt.verify(token, 'disdasecretyo', function(err, decoded){
+        if( err ){
+          return res.json({ success: false, message: 'Failed to authenticate token.' });
+        } else {
+          req.decoded = decoded; // if everything is good, then save to request for use in other routes
+          next();
+        }
+      });
+    } else { // if there is no token then return an error
+      return res.status(403).send({
+        success: false,
+        message: 'No token provided'
+      });
+    }
   },
 
   errorLogger: function(err, req, res, next){
