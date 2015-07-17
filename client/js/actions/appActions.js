c2a74e605f7b89cba229f88561944be65ae9d219
brettkan <bkfighton@gmail.com>
2015-07-17 11:03:48 -0700

    (refactor) remove start listeners function from appActions

--- client/js/actions/appActions.js
+++ client/js/actions/appActions.js
@@ -2,11 +2,11 @@ var AppDispatcher = require('../dispatchers/appDispatcher');
 var appConstants = require('../constants/appConstants');
 
 var appActions = {
-  startSocketListener: function() {
-    AppDispatcher.handleAction({
-      actionType: appConstants.START_SOCKET_LISTENER
-    });
-  }
+  // startSocketListener: function() {
+  //   AppDispatcher.handleAction({
+  //     actionType: appConstants.START_SOCKET_LISTENER
+  //   });
+  // }
 };
 
 module.exports = appActions;
