bc293accde9ad211ea26c373d807b121af424f79
Niall O'Kane <okanenm@gmail.com>
2015-07-16 18:47:08 -0700

    (feat/db) migrate organizations table

---
+++ server/migrations/20150716171608-create-organizations.js
@@ -0,0 +1,59 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.createTable('organizations', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      address: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      city: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      state: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      zip: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      country: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      industry: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      password_hash: {
+        type: Sequelize.STRING
+      },
+
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  down: function (queryInterface, Sequelize) {
+    return queryInterface.dropTable('organizations');
+  }
+};
