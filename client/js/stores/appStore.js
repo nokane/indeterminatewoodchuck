982614fa23f0e4f6fefd73e93f58599b4164b8e3
brettkan <bkfighton@gmail.com>
2015-07-17 10:19:27 -0700

    (refactor) refactor appStore.js to listen for JOIN_ROOM and set roomname based on JOIN_ROOM data

--- client/js/stores/appStore.js
+++ client/js/stores/appStore.js
@@ -4,15 +4,16 @@ var objectAssign = require('react/lib/Object.assign');
 var EventEmitter = require('events').EventEmitter;
 
 var CHANGE_EVENT = 'change';
-var SOCKET_LISTENER = 'SOCKET_LISTENER';
 
-var _connections = {
-  // should we require socket and icecomm with Browserify?
-  socket: io(),
-  comm: new Icecomm('ZZ2RA1DsHd9xdCqdoeJ8Wwra5A5fUKipAVrvzX6vOGHlLiAdO', { debug: true })
-};
+// var _connections = {
+//   comm: new Icecomm('ZZ2RA1DsHd9xdCqdoeJ8Wwra5A5fUKipAVrvzX6vOGHlLiAdO', { debug: true })
+// };
 
 var _room = '';
+
+var setRoom = function(room) {
+  _room = room;
+};
 
 var appStore = objectAssign({}, EventEmitter.prototype, {
   getRoom: function() {
@@ -20,19 +21,11 @@ var appStore = objectAssign({}, EventEmitter.prototype, {
   }
 });
 
-var startSocketListener = function(){
-  _connections.socket.on('staffRoom', function(msg) {
-    console.log('Staff connecting to room with name: ', msg);
-    _room = msg;
-    appStore.emit(CHANGE_EVENT);
-  });
-};
-
 AppDispatcher.register(function(payload) {
   var action = payload.action;
-  if (action.actionType === appConstants.START_SOCKET_LISTENER) {
-    startSocketListener();
-    appStore.emit(SOCKET_LISTENER);
+  if (action.actionType === appConstants.JOIN_ROOM) {
+    setRoom(action.data);
+    appStore.emit(CHANGE_EVENT);
   } else {
     return true;
   }
