b7d1fc7559aacd2f8bd8d631e047d2a44ffdcb6c
brettkan <bkfighton@gmail.com>
2015-07-16 19:47:51 -0700

    (feat) add appStore with dependencies, connections, roomname, and listener functions

---
+++ client/js/stores/appStore.js
@@ -0,0 +1,21 @@
+var AppDispatcher = require('../dispatcher/appDispatcher');
+var appConstants = require('../constants/appConstants');
+var objectAssign = require('react/lib/Object.assign');
+var EventEmitter = require('events').EventEmitter;
+
+var CHANGE_EVENT = 'change';
+
+var _connections = {
+  // should we require socket and icecomm with Browserify?
+  socket: io(),
+  comm: new Icecomm('ZZ2RA1DsHd9xdCqdoeJ8Wwra5A5fUKipAVrvzX6vOGHlLiAdO', { debug: true })
+};
+
+var _room = '';
+
+var setUpEventListeners = function(){
+  _connections.socket.on('staffRoom', function(msg) {
+    console.log('Staff connected to room with name: ', msg);
+    _room = msg;
+  }.bind(this));
+};
