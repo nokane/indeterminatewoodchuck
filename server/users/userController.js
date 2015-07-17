af0880249e26965efe8002693a50b92a5d87a9fb
Leon Yip <lyip1992@gmail.com>
2015-07-16 20:54:58 -0700

    (refactor) uncommented sign in

--- server/users/userController.js
+++ server/users/userController.js
@@ -4,27 +4,27 @@ var jwt = require('jsonwebtoken');
 
 module.exports = {
   signin: function(req, res, next){ // mongo implementation pending refactor to sequelize
-    // User.findOne({ name: req.body.name }, function(err, user){
-    //   if( err ) throw err;
-    //
-    //   if( !user ){
-    //     res.json({ success: false, message: 'Authentication failed. User not found.' });
-    //   } else if( user ){
-    //     if( user.password != req.body.password ){ // check if the password matches
-    //       res.json({ success: false, message: 'Authentication failed. Wrong password.' });
-    //     } else {
-    //       var token = jwt.sign(user, app.get('superSecret'), { // if a user is found and the password is right, create a token
-    //         expiresInMinutes: 1440 // expires in 24 hours
-    //       });
-    //
-    //       res.json({
-    //         success: true,
-    //         message: 'Enjoy your token!',
-    //         token: token
-    //       });
-    //     }
-    //   }
-    // });
+    User.findOne({ name: req.body.name }, function(err, user){
+      if( err ) throw err;
+
+      if( !user ){
+        res.json({ success: false, message: 'Authentication failed. User not found.' });
+      } else if( user ){
+        if( user.password != req.body.password ){ // check if the password matches
+          res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+        } else {
+          var token = jwt.sign(user, app.get('superSecret'), { // if a user is found and the password is right, create a token
+            expiresInMinutes: 1440 // expires in 24 hours
+          });
+
+          res.json({
+            success: true,
+            message: 'Enjoy your token!',
+            token: token
+          });
+        }
+      }
+    });
   },
 
   signup: function(req, res, next){
