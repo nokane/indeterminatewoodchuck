a3c5dc2fec259b4b26c7e4b8defd22fee6232929
Niall O'Kane <okanenm@gmail.com>
2015-07-16 19:08:11 -0700

    (feat/db) migrate staff table

---
+++ server/migrations/20150716185318-create-staff.js
@@ -0,0 +1,48 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.createTable('staff', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      org_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      first_name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      last_name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      password_hash: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  down: function (queryInterface, Sequelize) {
+    return queryInterface.dropTable('staff');
+  }
+};
