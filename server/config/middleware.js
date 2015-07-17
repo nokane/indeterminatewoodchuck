ec39c2a8be3427e930bf593a02e523d09a4ab832
Leon Yip <lyip1992@gmail.com>
2015-07-17 10:13:46 -0700

    (refactor) move middleware into config folder, move socketroute into config folder

---
+++ server/config/middleware.js
@@ -0,0 +1,22 @@
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+
+module.exports = function(app, express, server, io){
+  // var userRouter = express.Router();
+  // app.set('secret', 'disdasecretyo');
+
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.json());
+  app.use(morgan('dev'));
+
+  app.use(express.static(__dirname + '/../../client/dist'));
+  app.use('/library', express.static(__dirname + '/../../library'));
+
+  // app.use('/api/users', userRouter);
+  // require('../users/userRoutes.js')(userRouter);
+
+  server.socketroute = require('./socketroute.js');
+  io.on('connection', function(socket) {
+    server.socketroute.socketroute(io, socket);
+  });
+};
