f5051fe88998aea38871f4940ba70d2f5987f111
Leon Yip <lyip1992@gmail.com>
2015-07-18 12:11:17 -0700

    (refactor) comment out all functionality in signin and signup

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,32 +4,38 @@ var jwt = require('jsonwebtoken');
 
 module.exports = {
   signin: function(req, res, next){ // mongo implementation pending refactor to sequelize
-    User.findOne({ name: req.body.name }, function(err, user){
-      if( err ) throw err;
+    // User.findOne({ name: req.body.name }, function(err, user){
+    //   if( err ) throw err;
+    //
+    //   if( !user ){
+    //     res.json({ success: false, message: 'Authentication failed. User not found.' });
+    //   } else if( user ){
+    //     if( user.password !== req.body.password ){ // check if the password matches
+    //       res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+    //     } else {
+    //       var token = jwt.sign(user, 'disdasecretyo', { // if a user is found and the password is right, create a token
+    //         expiresInMinutes: 1440 // expires in 24 hours
+    //       });
+    //
+    //       res.json({
+    //         success: true,
+    //         message: 'Enjoy your token!',
+    //         token: token
+    //       });
+    //     }
+    //   }
+    // });
 
-      if( !user ){
-        res.json({ success: false, message: 'Authentication failed. User not found.' });
-      } else if( user ){
-        if( user.password != req.body.password ){ // check if the password matches
-          res.json({ success: false, message: 'Authentication failed. Wrong password.' });
-        } else {
-          var token = jwt.sign(user, 'disdasecretyo', { // if a user is found and the password is right, create a token
-            expiresInMinutes: 1440 // expires in 24 hours
-          });
-
-          res.json({
-            success: true,
-            message: 'Enjoy your token!',
-            token: token
-          });
-        }
-      }
-    });
+    res.send('Thank you for signing in!');
   },
 
   signup: function(req, res, next){
-    var username = req.body.username;
-    var password = req.body.password;
+    // first sign up the organization, THEN sign up the user
+
+    // var username = req.body.username;
+    // var password = req.body.password;
+
+    res.send('Thank you for signing up!');
 
     // if user is in the database
       // call next because the user is already in the database
