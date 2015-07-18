468e0ca5965e0d803010aef63f2b249d6cd212f3
Leon Yip <lyip1992@gmail.com>
2015-07-18 16:18:48 -0700

    (feat) add signupworg route on 'api/users'

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -20,15 +20,12 @@ module.exports = {
   },
 
   signup: function(req, res){
-    // first sign up the organization, THEN sign up the user
 
-    // var username = req.body.username;
-    // var password = req.body.password;
 
-    res.send('Thank you for signing up!');
+  },
 
-    // if user is in the database
-      // call next because the user is already in the database
-      // else create the new user and issue a new token
+  signupworg: function(req, res){
+
+
   }
 };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,4 +3,5 @@ var userController = require('../controllers/userController.js');
 module.exports = function(app){ // app is userRouter injected from middleware.js
   app.post('/signin', userController.signin);
   app.post('/signup', userController.signup);
+  app.post('/signuporg', userController.signupworg);
 };
