afe09aa5cecdadffbc63281e78df6769ecc43f0b
Niall O'Kane <okanenm@gmail.com>
2015-07-17 20:30:06 -0700

    (feat/spec) test: Create a new Session; find that Session with Session.findOne()

---
+++ spec/server/models/session.js
@@ -0,0 +1,49 @@
+require("dotenv").load();
+var expect = require("chai").expect;
+var express = require("express");
+var db = require("../../../server/models/index.js");
+
+describe("Session Model", function(){
+
+  beforeEach(function(done){
+    db.sequelize.sync({force: true}).then(function(){
+      done();
+    });
+  });
+
+  it("Create a new Session and find that Session with Session.findOne()", function(done){
+    var newOrg = db.Organization.build({name:"Bait Shop", 
+                    address: "123 fake street",
+                    city: "San Francisco",
+                    state: "CA",
+                    zip: "94122",
+                    country: "USA",
+                    industry: "apparel",
+                    password_hash: "bla"});
+    newOrg.save().then(function(x){
+      db.Organization.findOne({name:"Bait Shop"})
+        .then(function(org){
+        var user = db.User.build({first_name: "john",
+                      last_name: "doe",
+                      OrganizationId: org.id,
+                      title: "boss",
+                      email: "thefella@boss.com",
+                      password_hash: "righton"});
+        user.save().then(function() {
+          org.getUsers().then(function(users) {
+            var session = db.Session.build({room_name: "blue_room", 
+                            OrganizationId: org.id, 
+                            UserId: users[0].id});
+            session.save().then(function() {
+              db.Session.findOne({id: 1}).then(function(foundSession) {
+                expect(foundSession.room_name).to.equal("blue_room");
+                done();
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+
+});
