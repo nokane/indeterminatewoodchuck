c2c1d13a7415b23f724802b48abe780287d0c429
Niall O'Kane <okanenm@gmail.com>
2015-07-17 20:32:15 -0700

    (feat/seq) test: find Session with Organization.getSessions()

--- spec/server/models/session.js
+++ spec/server/models/session.js
@@ -12,7 +12,7 @@ describe("Session Model", function(){
   });
 
   it("Create a new Session and find that Session with Session.findOne()", function(done){
-    var newOrg = db.Organization.build({name:"Bait Shop", 
+    var newOrg = db.Organization.build({name:"Bait Shop",
                     address: "123 fake street",
                     city: "San Francisco",
                     state: "CA",
@@ -37,6 +37,41 @@ describe("Session Model", function(){
             session.save().then(function() {
               db.Session.findOne({id: 1}).then(function(foundSession) {
                 expect(foundSession.room_name).to.equal("blue_room");
+                done();
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+
+  it("Create a new Session and find that Session with Organization.getSessions()", function(done){
+    var newOrg = db.Organization.build({name:"Bait Shop",
+                    address: "123 fake street",
+                    city: "San Francisco",
+                    state: "CA",
+                    zip: "94122",
+                    country: "USA",
+                    industry: "apparel",
+                    password_hash: "bla"});
+    newOrg.save().then(function(x){
+      db.Organization.findOne({name:"Bait Shop"})
+        .then(function(org){
+        var user = db.User.build({first_name: "john",
+                      last_name: "doe",
+                      OrganizationId: org.id,
+                      title: "boss",
+                      email: "thefella@boss.com",
+                      password_hash: "righton"});
+        user.save().then(function() {
+          org.getUsers().then(function(users) {
+            var session = db.Session.build({room_name: "blue_room", 
+                            OrganizationId: org.id, 
+                            UserId: users[0].id});
+            session.save().then(function() {
+              org.getSessions().then(function(orgSession) {
+                expect(orgSession[0].room_name).to.equal("blue_room");
                 done();
               });
             });
