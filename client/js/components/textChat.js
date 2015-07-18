7458b64e0011dc2d799cc0b21e78f0222d17a879
brettkan <bkfighton@gmail.com>
2015-07-17 19:05:19 -0700

    (feat) add standard functions to textChat component

--- client/js/components/textChat.js
+++ client/js/components/textChat.js
@@ -3,15 +3,37 @@ var textChatStore = require('../stores/textChatStore');
 
 var TextChat = React.createClass({
   getInitialState: function() {
+    return {
+      messages: [];
+    }
+  },
 
+  componentDidMount: function(){
+    textChatStore.addChangeListener(this._onChange);
+  },
+
+  componentWillUnmount: function(){
+    textChatStore.removeChangeListener(this._onChange);    
   },
 
   _onChange: function() {
-
+    this.setState({
+      messages: textChatStore.getMessages()
+    })
   },
 
   render: function() {
-    
+    return (
+      <div className='send-chat'>
+        <form>
+          <input type='text' placeholder='Type your message here'>
+          <input type='submit'>
+        </form>
+      </div>
+      <div className='message-log'>
+
+      </div>
+      )
   }
 });
 
