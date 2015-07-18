da84e5fb62968a888e135410c27767871917e794
brettkan <bkfighton@gmail.com>
2015-07-17 18:56:06 -0700

    (feat) add basic scaffolding for textChat

--- client/js/components/textChat.js
+++ client/js/components/textChat.js
@@ -1,5 +1,18 @@
-var TextChat = React.createClass({
+var React = require('react');
+var textChatStore = require('../stores/textChatStore');
 
+var TextChat = React.createClass({
+  getInitialState: function() {
+
+  },
+
+  _onChange: function() {
+
+  },
+
+  render: function() {
+    
+  }
 });
 
 module.exports = TextChat;
---
+++ client/js/stores/textChatStore.js
@@ -0,0 +1,51 @@
+var AppDispatcher = require('../dispatchers/appDispatcher');
+var appConstants = require('../constants/appConstants');
+var objectAssign = require('react/lib/Object.assign');
+var EventEmitter = require('events').EventEmitter;
+
+var CHANGE = 'CHANGE';
+
+var _videoStreams = {
+  // local: null,
+  // remote: null
+};
+
+var setLocalStream = function(peer) {
+  // _videoStreams.local = peer;
+};
+
+var setRemoteStream = function(peer) {
+  // _videoStreams.remote = peer;
+};
+
+var videoChatStore = objectAssign({}, EventEmitter.prototype, {
+  // addChangeListener: function(cb){
+  //   this.on(CHANGE, cb);
+  // },
+  // removeChangeListener: function(cb){
+  //   this.removeListener(CHANGE, cb);
+  // },
+  // getLocalStream: function() {
+  //   return _videoStreams.local;
+  // },
+  // getRemoteStream: function() {
+  //   return _videoStreams.remote;
+  // }
+});
+
+videoChatStore.dispatchToken = AppDispatcher.register(function(payload) {
+  // if (payload.actionType === appConstants.START_LOCAL_CONN) {
+  //   setLocalStream(payload.peer);
+  //   videoChatStore.emit(CHANGE);
+  // } else if (payload.actionType === appConstants.START_REMOTE_CONN) {
+  //   setRemoteStream(payload.peer);
+  //   videoChatStore.emit(CHANGE);
+  // } else if (payload.actionType === appConstants.STOP_REMOTE_CONN) {
+  //   setRemoteStream(null);
+  //   videoChatStore.emit(CHANGE);
+  // } 
+
+  return true;
+});
+
+module.exports = videoChatStore;
