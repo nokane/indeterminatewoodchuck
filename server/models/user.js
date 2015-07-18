75c3a4f6f2d137c8274e4440dc2ec03a1391123d
Niall O'Kane <okanenm@gmail.com>
2015-07-18 13:22:41 -0700

    (feat/hash-pass) created 'hashPassword()' instanceMethod for User Model

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,6 @@
 'use strict';
+var bcrypt = require('bcrypt-nodejs');
+
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
     first_name: DataTypes.STRING,
@@ -11,6 +13,23 @@ module.exports = function(sequelize, DataTypes) {
       associate: function(models) {
         User.belongsTo(models.Organization);
         User.hasMany(models.Session);
+      }
+    },
+    instanceMethods: {
+      hashPassword: function(password, cb) {
+        var user = this;
+        return bcrypt.genSalt(10, function(err, salt) {
+          if (err) {
+            return cb(err);
+          }
+          return bcrypt.hash(password, salt, null, function(error, hash) {
+            if (err) {
+              return cb(err);
+            }
+            user.password_hash = hash;
+            return cb();
+          });
+        });
       }
     }
   });
