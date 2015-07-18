afe63a385d1a5ac8c2b61d96805b0260e6831ec9
Niall O'Kane <okanenm@gmail.com>
2015-07-17 21:17:37 -0700

    (feat/seq) first check if dotenv needs to be run

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var app = require('./server/server.js');
-
 if (process.env.PORT === undefined) {
   require('dotenv').load();
 }
+
+var app = require('./server/server.js');
 
 var port = process.env.PORT || 3000;
 
