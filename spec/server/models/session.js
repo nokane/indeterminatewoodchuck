d1bb4f63d565927fa3a1bc4beee310b6f42339dd
Niall O'Kane <okanenm@gmail.com>
2015-07-17 20:36:22 -0700

    (feat/spec) test: find Session with User.getSessions()

--- spec/server/models/session.js
+++ spec/server/models/session.js
@@ -81,4 +81,39 @@ describe("Session Model", function(){
     });
   });
 
+  it("Create a new Session and find that Session with User.getSessions()", function(done){
+    var newOrg = db.Organization.build({name:"Bait Shop",
+                    address: "123 fake street",
+                    city: "San Francisco",
+                    state: "CA",
+                    zip: "94122",
+                    country: "USA",
+                    industry: "apparel",
+                    password_hash: "bla"});
+    newOrg.save().then(function(x){
+      db.Organization.findOne({name:"Bait Shop"})
+        .then(function(org){
+        var user = db.User.build({first_name: "john",
+                      last_name: "doe",
+                      OrganizationId: org.id,
+                      title: "boss",
+                      email: "thefella@boss.com",
+                      password_hash: "righton"});
+        user.save().then(function() {
+          org.getUsers().then(function(users) {
+            var session = db.Session.build({room_name: "blue_room",
+                            OrganizationId: org.id,
+                            UserId: users[0].id});
+            session.save().then(function() {
+              users[0].getSessions().then(function(userSession) {
+                expect(userSession[0].room_name).to.equal("blue_room");
+                done();
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+
 });
