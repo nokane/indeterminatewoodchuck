0fe47cb76181877d2d33de875d87a6f909390355
Leon Yip <lyip1992@gmail.com>
2015-07-18 17:31:10 -0700

    (feat) build sign up route

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -7,7 +7,7 @@ module.exports = {
       if( !user ){
         res.json({ success: false, message: 'Authentication failed.' })
       } else {
-        user.comparePassword(req.body.password, function(valid){
+        user.checkPassword(req.body.password, function(valid){
           if( valid ){
             res.json({ success: false, message: 'Authentication failed.' });
           } else {
@@ -20,8 +20,28 @@ module.exports = {
   },
 
   signup: function(req, res){
-
-
+    db.Organization.findOne({ where: { name: req.body.businessName } }).then(function(org){
+      if( !org ){ res.json({ success: false, message: 'Organization does not exist.' }); }
+      org.checkPassword(req.body.businessPassword, function(valid){
+        if( !valid ){ res.json({ success: false, message: 'Wrong organization password.' }); }
+        else {
+          db.User.findOne({ where: { email: req.body.email } }).then(function(user){
+            if( user ){ res.json({ success: false, message: 'User already exists.' }); }
+            db.User.build({
+              first_name: req.body.firstName,
+              last_name: req.body.lastName,
+              OrganizationID: org.id,
+              title: req.body.jobTitle,
+              email: req.body.email,
+              password_hash: req.body.password
+            }).save().then(function(newUser){
+              var token = jwt.sign(user, 'disdasecretyo', { expiresInMinutes: 20 });
+              res.json({ success: true, message: 'Enjoy your token!', token: token });
+            });
+          });
+        }
+      });
+    });
   },
 
   signupworg: function(req, res){
