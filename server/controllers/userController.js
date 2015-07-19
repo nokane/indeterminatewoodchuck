5727976d558ae62e521f4ff12d3d975023f6b9fa
Leon Yip <lyip1992@gmail.com>
2015-07-18 19:48:25 -0700

    (feat) build signupworg controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,7 +5,7 @@ module.exports = {
   signin: function(req, res){
     db.User.findOne({ where: { email: req.body.email } }).then(function(user){
       if( !user ){
-        res.json({ success: false, message: 'Authentication failed.' })
+        res.json({ success: false, message: 'Authentication failed.' });
       } else {
         user.checkPassword(req.body.password, function(valid){
           if( valid ){
@@ -35,7 +35,7 @@ module.exports = {
               email: req.body.email,
               password_hash: req.body.password
             }).save().then(function(newUser){
-              var token = jwt.sign(user, 'disdasecretyo', { expiresInMinutes: 20 });
+              var token = jwt.sign(newUser, 'disdasecretyo', { expiresInMinutes: 20 });
               res.json({ success: true, message: 'Enjoy your token!', token: token });
             });
           });
@@ -45,7 +45,33 @@ module.exports = {
   },
 
   signupworg: function(req, res){
-
-
+    db.Organization.findOne({ where: { name: req.body.businessName } }).then(function(org){
+      if( org ){ res.json({ success: false, message: 'Organization already exists.' }); }
+      db.User.findOne({ where: { email: req.body.email } }).then(function(user){
+        if( user ){ res.json({ success: false, message: 'User already exists.' }); }
+        db.Organization.build({
+          name: req.body.businessName,
+          address: req.body.address,
+          city: req.body.city,
+          state: req.body.state,
+          zip: req.body.zip,
+          country: req.body.country,
+          industry: req.body.industry,
+          password_hash: req.body.businessPassword
+        }).save().then(function(newOrg){
+          db.User.build({
+            first_name: req.body.firstName,
+            last_name: req.body.lastName,
+            OrganizationID: newOrg.id,
+            title: req.body.jobTitle,
+            email: req.body.email,
+            password_hash: req.body.password
+          }).save().then(function(newUser){
+            var token = jwt.sign(newUser, 'disdasecretyo', { expiresInMinutes: 20 });
+            res.json({ success: true, message: 'Enjoy your token!', token: token });
+          });
+        });
+      });
+    });
   }
 };
