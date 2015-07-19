a9b9b2dcdfea9ced0bbc21b3be7b04b1cb525694
Leon Yip <lyip1992@gmail.com>
2015-07-19 15:17:20 -0700

    (refactor) remove superagent

--- spec/server/controllers/userControllerSpec.js
+++ spec/server/controllers/userControllerSpec.js
@@ -1,10 +1,7 @@
 require('dotenv').load();
 var db = require('../../../server/models/index.js');
 var request = require('supertest');
-var superagent = require('superagent');
 var app = require('../../../index.js');
-
-var agent = superagent.agent();
 
 describe('User Controller', function(){
 
