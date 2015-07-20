37d6be535bec60aea48cd9906eaab19506ea73eb
John Paulino <paulinoj@gmail.com>
2015-07-20 00:49:39 -0700

    (feat/loginPage) add React child component for links that allow either organization signin or signup

---
+++ client/js/components/login/orgAccountQuery.js
@@ -0,0 +1,27 @@
+var React = require('react');
+
+var OrgAccountQuery = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <table>
+          <td><a href='#' className='orgLogin' onClick={this.handleShowOrgLogin}>Join Existing Organization</a></td>  
+          <td><a href='#' className='orgSignup' onClick={this.handleShowOrgSignup}>Create Organization</a></td>
+        </table>
+      </div>
+    )
+  },
+
+  handleShowOrgLogin: function(event) {
+    event.preventDefault();
+    this.props.showOrgLogin();
+
+  },
+
+  handleShowOrgSignup: function(event) {
+    event.preventDefault();
+    this.props.showOrgSignup();
+  }
+});
+
+module.exports = OrgAccountQuery;
