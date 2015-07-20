4b299ca20c0966b194db4ef873bd48eabf1c0d0b
Niall O'Kane <okanenm@gmail.com>
2015-07-20 15:15:02 -0700

    (feat/db-field) add field to Organizations table: web_name

--- server/migrations/20150717122446-create-organizations.js
+++ server/migrations/20150717122446-create-organizations.js
@@ -32,6 +32,9 @@ module.exports = {
       password_hash: {
         type: Sequelize.STRING
       },
+      web_name: {
+        type: Sequelize.STRING
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
