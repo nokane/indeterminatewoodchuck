ac495e2a17685c2e32e7386b69e379ec1d78acd6
Leon Yip <lyip1992@gmail.com>
2015-07-19 19:48:32 -0700

    (refactor) insert else blocks

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -52,33 +52,37 @@ module.exports = {
   signupwithorg: function(req, res){
     db.Organization.findOne({ where: { name: req.body.businessName } }).then(function(org){
       if( org ){ res.json({ success: false, message: 'Organization already exists.' }); }
-      db.User.findOne({ where: { email: req.body.email } }).then(function(user){
-        if( user ){ res.json({ success: false, message: 'User already exists.' }); }
-        var orgBuild = db.Organization.build({
-          name: req.body.businessName,
-          address: req.body.address,
-          city: req.body.city,
-          state: req.body.state,
-          zip: req.body.zip,
-          country: req.body.country,
-          industry: req.body.industry,
-          password_hash: req.body.businessPassword
+      else {
+        db.User.findOne({ where: { email: req.body.email } }).then(function(user){
+          if( user ){ res.json({ success: false, message: 'User already exists.' }); }
+          else {
+            var orgBuild = db.Organization.build({
+              name: req.body.businessName,
+              address: req.body.address,
+              city: req.body.city,
+              state: req.body.state,
+              zip: req.body.zip,
+              country: req.body.country,
+              industry: req.body.industry,
+              password_hash: req.body.businessPassword
+            });
+            orgBuild.save().then(function(newOrg){
+              var userBuild = db.User.build({
+                first_name: req.body.firstName,
+                last_name: req.body.lastName,
+                OrganizationID: newOrg.id,
+                title: req.body.jobTitle,
+                email: req.body.email,
+                password_hash: req.body.password
+              });
+              userBuild.save().then(function(newUser){
+                var token = jwt.sign(newUser, 'disdasecretyo', { expiresInMinutes: 20 });
+                res.json({ success: true, message: 'Enjoy your token!', token: token });
+              });
+            });
+          }
         });
-        orgBuild.save().then(function(newOrg){
-          var userBuild = db.User.build({
-            first_name: req.body.firstName,
-            last_name: req.body.lastName,
-            OrganizationID: newOrg.id,
-            title: req.body.jobTitle,
-            email: req.body.email,
-            password_hash: req.body.password
-          })
-          userBuild.save().then(function(newUser){
-            var token = jwt.sign(newUser, 'disdasecretyo', { expiresInMinutes: 20 });
-            res.json({ success: true, message: 'Enjoy your token!', token: token });
-          });
-        });
-      });
+      }
     });
   }
 };
