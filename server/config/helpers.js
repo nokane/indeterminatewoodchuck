ee22f393493d03e49befec03c9c39ffda8605dac
Leon Yip <lyip1992@gmail.com>
2015-07-21 16:06:48 -0700

    (refactor) send redirect to login instead of json object

--- server/config/helpers.js
+++ server/config/helpers.js
@@ -5,21 +5,25 @@ module.exports = {
   checkAuth: function(req, res, next){
     console.log('You are trying to access a protected route.');
     var token = req.query.token;
+    console.log('This is the token: ', token);
+
 
     if( token ){
       jwt.verify(token, 'disdasecretyo', function(err, decoded){
         if( err ){
-          return res.json({ success: false, message: 'Failed to authenticate token.' });
+          console.log('The token provided was invalid, redirecting to login.');
+          res.redirect('/login');
+          // return res.json({ success: false, message: 'Failed to authenticate token.' });
         } else {
           req.decoded = decoded; // if everything is good, then save to request for use in other routes
+          console.log('Your token is good to go. You may access protected assets.');
           next();
         }
       });
     } else { // if there is no token then return an error
-      return res.status(403).send({
-        success: false,
-        message: 'No token provided'
-      });
+      // return res.status(403).send({ success: false, message: 'No token provided' });
+      console.log('No token provided, redirecting to login.');
+      res.redirect('/login');
     }
   },
 
