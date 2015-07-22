b9fec0049d386a71f3e3073029f48760a6e1d3ea
brettkan <bkfighton@gmail.com>
2015-07-22 15:26:42 -0700

    (bug) fix server organization handling

--- server/controllers/employeeDataController.js
+++ server/controllers/employeeDataController.js
@@ -2,7 +2,7 @@ var db = require('../models/index.js');
 
 module.exports = {
   getEmployeeData: function(req, res){
-    db.Organization.findOne({ id: req.decoded.OrganizationId }).then(function(org){
+    db.Organization.findOne({ where: { id: req.decoded.OrganizationId } }).then(function(org){
       res.json({
         web_name: org.web_name,
         employeeId: req.decoded.id,
