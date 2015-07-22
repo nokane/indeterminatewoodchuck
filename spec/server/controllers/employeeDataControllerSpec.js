6c70ce1400fd558e24a99cbd0aaf899216afc288
Leon Yip <lyip1992@gmail.com>
2015-07-22 10:19:20 -0700

    (test) set up employee data controller scaffolding

---
+++ spec/server/controllers/employeeDataControllerSpec.js
@@ -0,0 +1,11 @@
+require('dotenv').load();
+var db = require('../../../server/models/index.js');
+var expect = require('chai').expect;
+var request = require('supertest');
+var app = require('../../../index.js');
+
+describe('Employee Data Controller', function(){
+
+  
+
+});
