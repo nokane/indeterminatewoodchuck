a23690d8d221868e2faa0bfa31882e1a10e689d9
Leon Yip <lyip1992@gmail.com>
2015-07-22 10:40:26 -0700

    (feat) add test

--- spec/server/controllers/employeeDataControllerSpec.js
+++ spec/server/controllers/employeeDataControllerSpec.js
@@ -6,6 +6,9 @@ var app = require('../../../index.js');
 
 describe('Employee Data Controller', function(){
 
-  
+
+  it('should be able to get employee data when a user is logged in', function(done){
+    // the respone should contain the stuff in that object
+  });
 
 });
