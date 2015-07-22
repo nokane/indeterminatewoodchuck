0a9da9a56f38910a4005893c62c40b5cf9c81f6d
Leon Yip <lyip1992@gmail.com>
2015-07-21 19:45:19 -0700

    (feat) set up employeeDataController scaffolding

---
+++ server/controllers/employeeDataController.js
@@ -0,0 +1,9 @@
+var db = require('../models/index.js');
+var jwt = require('jsonwebtoken');
+var helpers = require('../config/helpers.js');
+
+module.exports = {
+  getEmployeeData: function(req, res){
+    console.log('Getting employee data!');
+  }
+};
