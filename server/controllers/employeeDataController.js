1de997b103b1497904a6d37587dc912d40a2c3b0
Leon Yip <lyip1992@gmail.com>
2015-07-21 21:04:44 -0700

    (feat) send decoded employee data along with organization web name

--- server/controllers/employeeDataController.js
+++ server/controllers/employeeDataController.js
@@ -1,9 +1,16 @@
 var db = require('../models/index.js');
-var jwt = require('jsonwebtoken');
-var helpers = require('../config/helpers.js');
 
 module.exports = {
   getEmployeeData: function(req, res){
-    console.log('Getting employee data!');
+    db.Organization.findOne({ id: req.decoded.OrganizationId }).then(function(org){
+      res.json({
+        web_name: org.web_name,
+        employeeId: req.decoded.id,
+        employeeFirstName: req.decoded.first_name,
+        employeeLastName: req.decoded.last_name,
+        employeeTitle: req.decoded.title,
+        employeeEmail: req.decoded.email
+      });
+    });
   }
 };
