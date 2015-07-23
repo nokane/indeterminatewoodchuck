7e30b917effda374413c3adc3f4049c707ce3733
Leon Yip <lyip1992@gmail.com>
2015-07-22 19:39:25 -0700

    (feat) add logout logic on portal for view and actions

--- client/portal/js/actions/appActions.js
+++ client/portal/js/actions/appActions.js
@@ -19,6 +19,18 @@ var appActions = {
         }
         setEmployeeData(res.body);
       });
+  },
+
+  employeeSignOut: function(){
+    // delete cookie
+    // go to apiUtils and issue a get request to '/login'
+    apiUtil.logout()
+      .end(function(err, res){
+        if( err ){
+          console.log('There was a problem logging out: ', err);
+          return;
+        }
+      });
   }
 };
 
--- client/portal/js/components/navBar.js
+++ client/portal/js/components/navBar.js
@@ -1,12 +1,20 @@
 var React = require('react');
+var appActions = require('../actions/appActions');
 
 var NavBar = React.createClass({
+
+  handleSignOut: function(){
+    appActions.employeeSignOut();
+  },
 
   render: function() {
     return (
       <nav className='navbar navbar-default'>
         <div className='navbar-brand'>Supportal</div>
         <div className='navbar-collapse collapse'>
+          <ul className='nav navbar-right'>
+            <li onClick={this.handleSignOut}>Sign Out</li>
+          </ul>
           <ul className='nav navbar-right'>
             <li>User logged in as: {this.props.firstName} {this.props.lastName}</li>
             <li>Email: {this.props.email}</li>
