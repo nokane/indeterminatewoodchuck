0e9d8ee3f477394f04c016a59dd2ddbd71687481
Niall O'Kane <okanenm@gmail.com>
2015-07-28 12:28:01 -0700

    (feat) define render()

--- client/portal/js/components/chatInterface.js
+++ client/portal/js/components/chatInterface.js
@@ -2,9 +2,29 @@ var React = require('react');
 var VideoChat = require('./videoChat');
 var Queue = require('./queue');
 var TextChat = require('./textChat');
+var appStore = require('../stores/appStore');
+var appActions = require('../actions/appActions');
+
 
 var ChatInterface = React.createClass({
+  render: function() {
 
+    return (
+      <div>
+        <div className='row'>
+          <div className='column1'>
+            <VideoChat />
+          </div>
+          <div className='column2'>
+            <Queue web_name={web_name} />
+          </div>
+        </div>
+        <div className='row'>
+          <TextChat />
+        </div>
+    </div>
+    );
+  }
 });
 
 module.exports = ChatInterface;
