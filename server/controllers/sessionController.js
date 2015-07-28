0d2b5f8880dea1c65255529c0442ca57dc9a3216
Niall O'Kane <okanenm@gmail.com>
2015-07-27 18:43:02 -0700

    (refactor) create addSession()

--- server/controllers/sessionController.js
+++ server/controllers/sessionController.js
@@ -1,3 +1,17 @@
 var db = require('../models/index.js');
 
-module.exports = {};
+module.exports = {
+  addSession = function(staffId, roomname) {
+    db.User.findOne({id: Number(staffId)}).then(function(user) {
+      db.Session.build({
+        UserId: user.id,
+        OrganizationId: user.OrganizationId,
+        room_name: roomname
+      }).
+      db.Session.save().then(function(session) {
+        console.log("SESSION successfully created!");
+        console.log(session);
+      })
+    })
+  }
+};
