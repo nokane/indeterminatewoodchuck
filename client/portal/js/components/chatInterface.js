42b71653ccfa7a91c24706e7d8af0ca3a4c75099
Niall O'Kane <okanenm@gmail.com>
2015-07-28 11:50:17 -0700

    (feat) initialize chatInterface.js

---
+++ client/portal/js/components/chatInterface.js
@@ -0,0 +1,10 @@
+var React = require('react');
+var VideoChat = require('./videoChat');
+var Queue = require('./queue');
+var TextChat = require('./textChat');
+
+var ChatInterface = React.createClass({
+
+});
+
+module.exports = ChatInterface;
