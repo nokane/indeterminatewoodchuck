27e45ee31a34ee11d4dee1596353a0ddbcbfcd43
John Paulino <paulinoj@gmail.com>
2015-07-29 10:37:24 -0700

    (feature/get_session_data) add controller function that retrieves session data from database

--- server/controllers/employeeDataController.js
+++ server/controllers/employeeDataController.js
@@ -1,7 +1,7 @@
 var db = require('../models/index.js');
 
 module.exports = {
-  getEmployeeData: function(req, res){
+  getEmployeeUserData: function(req, res){
     db.Organization.findOne({ where: { id: req.decoded.OrganizationId } }).then(function(org){
       res.json({
         web_name: org.web_name,
@@ -12,5 +12,17 @@ module.exports = {
         employeeEmail: req.decoded.email
       });
     });
+  },
+
+  getEmployeeSessionData: function(req, res){
+    db.Session.findAll({attributes: ['cust_name', 'cust_email', 'createdAt', 'UserId', sequelize.col('User.title')], where: { OrganizationId: req.decoded.OrganizationId }, 
+      include: [ { model: db.Organization, attributes: ['name'] },
+                 { model: db.User, attributes: ['first_name', 'last_name', 'email', 'title'] }
+               ] }).then(function(sessions){
+      console.log("SESSION");
+      console.log(JSON.stringify(sessions));
+      res.json({john: "whatever"});
+    });
   }
+
 };
