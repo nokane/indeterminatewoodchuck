4de8bfdb09f5463d34b5fb6fc3ab1dfa4bc7ce91
Leon Yip <lyip1992@gmail.com>
2015-07-29 16:32:14 -0700

    (feat) parse date format

--- client/portal/js/components/sessionHistory.js
+++ client/portal/js/components/sessionHistory.js
@@ -45,3 +45,14 @@ var SessionHistory = React.createClass({
 });
 
 module.exports = SessionHistory;
+
+/* [ { cust_name: 'first customer', -
+   cust_email: 'firstcustomer@gmail.com', -
+   createdAt: '2015-07-29T17:50:38.782Z', -
+   question: 'whatever', -
+   UserId: 1,
+   name: 'Hack Reactor',
+   first_name: 'John', -
+   last_name: 'Paulino', -
+   email: 'paulinoj@gmail.com',
+   title: 'Software Engineer' }] - */
