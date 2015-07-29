6b99fcbe4192ea1b0b170f682f89efdac8054225
Leon Yip <lyip1992@gmail.com>
2015-07-29 14:59:51 -0700

    (fix) update formatting

--- client/portal/js/components/sessionLog.js
+++ client/portal/js/components/sessionLog.js
@@ -1,26 +1,12 @@
 var React = require('react');
 
-/* [ { cust_name: 'first customer',
-   cust_email: 'firstcustomer@gmail.com',
-   createdAt: '2015-07-29T17:50:38.782Z',
-   question: 'whatever',
-   UserId: 1,
-   name: 'Hack Reactor',
-   first_name: 'John',
-   last_name: 'Paulino',
-   email: 'paulinoj@gmail.com',
-   title: 'Software Engineer' }] */
-
 var SessionLog = React.createClass({
 
   render: function(){
-    console.log('These are the props: ', this.props);
-
-    var sessions = this.props.sessions.map(function(session){
+    var sessions = this.props.sessions.map(function(session, index){
       return (
-        <tr>
+        <tr key={ index }>
           <td>{ session.createdAt }</td>
-          <td>{ session.UserId }</td>
           <td>{ session.first_name + ' ' + session.last_name }</td>
           <td>{ session.cust_name }</td>
           <td>{ session.cust_email }</td>
@@ -45,8 +31,7 @@ var SessionLog = React.createClass({
         <caption>Session History Log</caption>
         <thead>
           <tr>
-            <th>Time</th>
-            <th>User ID</th>
+            <th>Date/Time</th>
             <th>Employee Name</th>
             <th>Customer Name</th>
             <th>Email</th>
