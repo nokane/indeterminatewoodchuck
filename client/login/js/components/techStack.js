da36fefe296798b77f3af15dcc991334351eff0b
Leon Yip <lyip1992@gmail.com>
2015-07-30 16:57:01 -0700

    (feat) tech stack scaffolding

---
+++ client/login/js/components/techStack.js
@@ -0,0 +1,97 @@
+var React = require('react');
+
+var TechStack = React.createClass({
+
+  render: function(){
+    return (
+      <div className="container">
+
+        <div className="row">
+
+            <div className="col-lg-12">
+                <h1 className="page-header">Thumbnail Gallery</h1>
+            </div>
+
+            <div className="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a className="thumbnail" href="#">
+                    <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
+                </a>
+            </div>
+            <div className="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a className="thumbnail" href="#">
+                    <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
+                </a>
+            </div>
+            <div className="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a className="thumbnail" href="#">
+                    <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
+                </a>
+            </div>
+            <div className="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a className="thumbnail" href="#">
+                    <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
+                </a>
+            </div>
+            <div className="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a className="thumbnail" href="#">
+                    <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
+                </a>
+            </div>
+            <div className="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a className="thumbnail" href="#">
+                    <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
+                </a>
+            </div>
+            <div className="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a className="thumbnail" href="#">
+                    <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
+                </a>
+            </div>
+            <div className="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a className="thumbnail" href="#">
+                    <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
+                </a>
+            </div>
+            <div className="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a className="thumbnail" href="#">
+                    <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
+                </a>
+            </div>
+            <div className="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a className="thumbnail" href="#">
+                    <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
+                </a>
+            </div>
+            <div className="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a className="thumbnail" href="#">
+                    <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
+                </a>
+            </div>
+            <div className="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a className="thumbnail" href="#">
+                    <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
+                </a>
+            </div>
+        </div>
+      </div>
+    );
+  }
+
+});
+
+module.exports = TechStack;
+
+/*
+Node / Express
+PostgreSQL
+Sequelize
+Socket.io
+WebRTC
+Icecomm
+React / Flux
+Heroku
+Gulp
+Chai
+TravisCI
+jwt
+*/
