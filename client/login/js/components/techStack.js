ae412d3107abf9cc990ba4b9da644049215e3116
Leon Yip <lyip1992@gmail.com>
2015-07-30 21:03:40 -0700

    (feat) center images

--- client/login/js/components/techStack.js
+++ client/login/js/components/techStack.js
@@ -12,70 +12,70 @@ var TechStack = React.createClass({
           </div>
 
           <div className="row">
-            <div className="col-lg-3 col-md-4 col-xs-6 thumb">
+            <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
               <a className="thumbnail" href="#">
-                <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
+                <img className="img-responsive" src="./assets/techStack/nodejs.png" alt="" />
               </a>
             </div>
-            <div className="col-lg-3 col-md-4 col-xs-6 thumb">
+            <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
               <a className="thumbnail" href="#">
-                <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
+                <img className="img-responsive" src="./assets/techStack/express.png" alt="" />
               </a>
             </div>
-            <div className="col-lg-3 col-md-4 col-xs-6 thumb">
+            <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
               <a className="thumbnail" href="#">
-                <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
+                <img className="img-responsive" src="./assets/techStack/postgres.png" alt="" />
               </a>
             </div>
-            <div className="col-lg-3 col-md-4 col-xs-6 thumb">
+            <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
               <a className="thumbnail" href="#">
-                <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
-              </a>
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="col-lg-3 col-md-4 col-xs-6 thumb">
-              <a className="thumbnail" href="#">
-                <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
-              </a>
-            </div>
-            <div className="col-lg-3 col-md-4 col-xs-6 thumb">
-              <a className="thumbnail" href="#">
-                <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
-              </a>
-            </div>
-            <div className="col-lg-3 col-md-4 col-xs-6 thumb">
-              <a className="thumbnail" href="#">
-                <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
-              </a>
-            </div>
-            <div className="col-lg-3 col-md-4 col-xs-6 thumb">
-              <a className="thumbnail" href="#">
-                <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
+                <img className="img-responsive" src="./assets/techStack/sequelize.png" alt="" />
               </a>
             </div>
           </div>
 
           <div className="row">
-            <div className="col-lg-3 col-md-4 col-xs-6 thumb">
+            <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
               <a className="thumbnail" href="#">
-                <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
+                <img className="img-responsive" src="./assets/techStack/socketio.png" alt="" />
               </a>
             </div>
-            <div className="col-lg-3 col-md-4 col-xs-6 thumb">
+            <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
               <a className="thumbnail" href="#">
-                <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
+                <img className="img-responsive" src="./assets/techStack/icecomm.png" alt="" />
               </a>
             </div>
-            <div className="col-lg-3 col-md-4 col-xs-6 thumb">
+            <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
               <a className="thumbnail" href="#">
-                <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
+                <img className="img-responsive" src="./assets/techStack/mocha.png" alt="" />
               </a>
             </div>
-            <div className="col-lg-3 col-md-4 col-xs-6 thumb">
+            <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
               <a className="thumbnail" href="#">
-                <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
+                <img className="img-responsive" src="./assets/techStack/heroku.png" alt="" />
+              </a>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
+              <a className="thumbnail" href="#">
+                <img className="img-responsive" src="./assets/techStack/travis.png" alt="" />
+              </a>
+            </div>
+            <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
+              <a className="thumbnail" href="#">
+                <img className="img-responsive" src="./assets/techStack/react-flux.png" alt="" />
+              </a>
+            </div>
+            <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
+              <a className="thumbnail" href="#">
+                <img className="img-responsive" src="./assets/techStack/github.png" alt="" />
+              </a>
+            </div>
+            <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
+              <a className="thumbnail" href="#">
+                <img className="img-responsive" src="./assets/techStack/gulp.jpg" alt="" />
               </a>
             </div>
           </div>
