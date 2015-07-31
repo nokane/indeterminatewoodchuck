a3c66a5fc26fc01c5e25fb1257654936f674d315
Leon Yip <lyip1992@gmail.com>
2015-07-31 16:18:40 -0700

    (refactor) rearrange images in terms of importance

--- client/login/js/components/techStack.js
+++ client/login/js/components/techStack.js
@@ -5,80 +5,79 @@ var TechStack = React.createClass({
   render: function(){
     return (
       <div className="row">
+        <div className="col-lg-12 text-center">
+          <h2 className="heading">Tech Stack</h2>
+          <hr className="primary divider"></hr>
+        </div>
 
-          <div className="col-lg-12 text-center">
-              <h2 className="heading">Tech Stack</h2>
-              <hr className="primary divider"></hr>
+        <div className="row">
+          <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
+            <a className="thumbnail" href="https://nodejs.org/">
+              <img className="img-responsive" src="./assets/techStack/nodejs.png" alt="Node.js" />
+            </a>
           </div>
+          <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
+            <a className="thumbnail" href="http://expressjs.com/">
+              <img className="img-responsive" src="./assets/techStack/express.png" alt="Express.js" />
+            </a>
+          </div>
+          <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
+            <a className="thumbnail" href="http://www.postgresql.org/">
+              <img className="img-responsive" src="./assets/techStack/postgres.png" alt="PostgreSQL" />
+            </a>
+          </div>
+          <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
+            <a className="thumbnail" href="http://docs.sequelizejs.com/en/latest/">
+              <img className="img-responsive" src="./assets/techStack/sequelize.png" alt="Sequelize" />
+            </a>
+          </div>
+        </div>
 
-          <div className="row">
-            <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
-              <a className="thumbnail" href="https://nodejs.org/">
-                <img className="img-responsive" src="./assets/techStack/nodejs.png" alt="" />
-              </a>
-            </div>
-            <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
-              <a className="thumbnail" href="http://expressjs.com/">
-                <img className="img-responsive" src="./assets/techStack/express.png" alt="" />
-              </a>
-            </div>
-            <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
-              <a className="thumbnail" href="http://www.postgresql.org/">
-                <img className="img-responsive" src="./assets/techStack/postgres.png" alt="" />
-              </a>
-            </div>
-            <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
-              <a className="thumbnail" href="http://docs.sequelizejs.com/en/latest/">
-                <img className="img-responsive" src="./assets/techStack/sequelize.png" alt="" />
-              </a>
-            </div>
+        <div className="row">
+          <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
+            <a className="thumbnail" href="http://socket.io/">
+              <img className="img-responsive" src="./assets/techStack/socketio.png" alt="Socket.io" />
+            </a>
           </div>
+          <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
+            <a className="thumbnail" href="http://icecomm.io/">
+              <img className="img-responsive" src="./assets/techStack/icecomm.png" alt="Icecomm" />
+            </a>
+          </div>
+          <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
+            <a className="thumbnail" href="http://facebook.github.io/react/">
+              <img className="img-responsive" src="./assets/techStack/react.png" alt="React.js" />
+            </a>
+          </div>
+          <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
+            <a className="thumbnail" href="https://facebook.github.io/flux/">
+              <img className="img-responsive" src="./assets/techStack/flux.png" alt="Flux.js" />
+            </a>
+          </div>
+        </div>
 
-          <div className="row">
-            <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
-              <a className="thumbnail" href="http://socket.io/">
-                <img className="img-responsive" src="./assets/techStack/socketio.png" alt="" />
-              </a>
-            </div>
-            <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
-              <a className="thumbnail" href="http://icecomm.io/">
-                <img className="img-responsive" src="./assets/techStack/icecomm.png" alt="" />
-              </a>
-            </div>
-            <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
-              <a className="thumbnail" href="http://mochajs.org/">
-                <img className="img-responsive" src="./assets/techStack/mocha.png" alt="" />
-              </a>
-            </div>
-            <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
-              <a className="thumbnail" href="https://www.heroku.com/">
-                <img className="img-responsive" src="./assets/techStack/heroku.jpg" alt="" />
-              </a>
-            </div>
+        <div className="row">
+          <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
+            <a className="thumbnail" href="http://mochajs.org/">
+              <img className="img-responsive" src="./assets/techStack/mocha.png" alt="Mocha.js" />
+            </a>
           </div>
-
-          <div className="row">
-            <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
-              <a className="thumbnail" href="https://travis-ci.org/">
-                <img className="img-responsive" src="./assets/techStack/travis.png" alt="" />
-              </a>
-            </div>
-            <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
-              <a className="thumbnail" href="http://facebook.github.io/react/">
-                <img className="img-responsive" src="./assets/techStack/react.png" alt="" />
-              </a>
-            </div>
-            <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
-              <a className="thumbnail" href="https://facebook.github.io/flux/">
-                <img className="img-responsive" src="./assets/techStack/flux.png" alt="" />
-              </a>
-            </div>
-            <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
-              <a className="thumbnail" href="http://gulpjs.com/">
-                <img className="img-responsive" src="./assets/techStack/gulp.png" alt="" />
-              </a>
-            </div>
+          <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
+            <a className="thumbnail" href="http://gulpjs.com/">
+              <img className="img-responsive" src="./assets/techStack/gulp.png" alt="Gulp.js" />
+            </a>
           </div>
+          <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
+            <a className="thumbnail" href="https://travis-ci.org/">
+              <img className="img-responsive" src="./assets/techStack/travis.png" alt="TravisCI" />
+            </a>
+          </div>
+          <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
+            <a className="thumbnail" href="https://www.heroku.com/">
+              <img className="img-responsive" src="./assets/techStack/heroku.jpg" alt="Heroku" />
+            </a>
+          </div>
+        </div>
       </div>
     );
   }
