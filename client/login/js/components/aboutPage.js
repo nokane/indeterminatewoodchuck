2d429de7ea7a18e20482522975aed023f4f4e769
Niall O'Kane <okanenm@gmail.com>
2015-07-31 16:21:51 -0700

    (feat) render Intro and Features React components

--- client/login/js/components/aboutPage.js
+++ client/login/js/components/aboutPage.js
@@ -1,12 +1,18 @@
 var React = require('react');
 var About = require('./about');
 var TechStack = require('./techStack');
+var Intro = require('./intro.js');
+var Features = require('./features.js');
 
 var AboutPage = React.createClass({
 
   render: function() {
     return (
       <div>
+        <Intro />
+        <hr className="primary"></hr>
+        <Features />
+        <hr className="primary"></hr>
         <TechStack />
         <hr className="primary"></hr>
         <About />
