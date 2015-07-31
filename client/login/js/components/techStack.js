b4ddb8c63a6e9f1d82110f9a61c6ca6fedbd61c1
Leon Yip <lyip1992@gmail.com>
2015-07-30 21:15:00 -0700

    (feat) remove border from images

--- client/login/js/components/techStack.js
+++ client/login/js/components/techStack.js
@@ -13,22 +13,22 @@ var TechStack = React.createClass({
 
           <div className="row">
             <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
-              <a className="thumbnail" href="#">
+              <a className="thumbnail borderless" href="#">
                 <img className="img-responsive" src="./assets/techStack/nodejs.png" alt="" />
               </a>
             </div>
             <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
-              <a className="thumbnail" href="#">
+              <a className="thumbnail borderless" href="#">
                 <img className="img-responsive" src="./assets/techStack/express.png" alt="" />
               </a>
             </div>
             <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
-              <a className="thumbnail" href="#">
+              <a className="thumbnail borderless" href="#">
                 <img className="img-responsive" src="./assets/techStack/postgres.png" alt="" />
               </a>
             </div>
             <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
-              <a className="thumbnail" href="#">
+              <a className="thumbnail borderless" href="#">
                 <img className="img-responsive" src="./assets/techStack/sequelize.png" alt="" />
               </a>
             </div>
@@ -36,22 +36,22 @@ var TechStack = React.createClass({
 
           <div className="row">
             <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
-              <a className="thumbnail" href="#">
+              <a className="thumbnail borderless" href="#">
                 <img className="img-responsive" src="./assets/techStack/socketio.png" alt="" />
               </a>
             </div>
             <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
-              <a className="thumbnail" href="#">
+              <a className="thumbnail borderless" href="#">
                 <img className="img-responsive" src="./assets/techStack/icecomm.png" alt="" />
               </a>
             </div>
             <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
-              <a className="thumbnail" href="#">
+              <a className="thumbnail borderless" href="#">
                 <img className="img-responsive" src="./assets/techStack/mocha.png" alt="" />
               </a>
             </div>
             <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
-              <a className="thumbnail" href="#">
+              <a className="thumbnail borderless" href="#">
                 <img className="img-responsive" src="./assets/techStack/heroku.jpg" alt="" />
               </a>
             </div>
@@ -59,22 +59,22 @@ var TechStack = React.createClass({
 
           <div className="row">
             <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
-              <a className="thumbnail" href="#">
+              <a className="thumbnail borderless" href="#">
                 <img className="img-responsive" src="./assets/techStack/travis.png" alt="" />
               </a>
             </div>
             <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
-              <a className="thumbnail" href="#">
+              <a className="thumbnail borderless" href="#">
                 <img className="img-responsive" src="./assets/techStack/react-flux.png" alt="" />
               </a>
             </div>
             <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
-              <a className="thumbnail" href="#">
+              <a className="thumbnail borderless" href="#">
                 <img className="img-responsive" src="./assets/techStack/github.png" alt="" />
               </a>
             </div>
             <div className="col-lg-3 col-md-4 col-xs-6 thumb vcenter">
-              <a className="thumbnail" href="#">
+              <a className="thumbnail borderless" href="#">
                 <img className="img-responsive" src="./assets/techStack/gulp.png" alt="" />
               </a>
             </div>
