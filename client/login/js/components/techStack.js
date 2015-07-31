b2ad3698df7cd6176e8717eb3f3ae284858d3b42
Leon Yip <lyip1992@gmail.com>
2015-07-30 20:48:09 -0700

    (feat) put images in rows of 4

--- client/login/js/components/techStack.js
+++ client/login/js/components/techStack.js
@@ -11,65 +11,73 @@ var TechStack = React.createClass({
               <hr className="primary"></hr>
           </div>
 
-          <div className="col-lg-3 col-md-4 col-xs-6 thumb">
-              <a className="thumbnail" href="#">
-                  <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
-              </a>
+          <div className="row">
+            <div className="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a className="thumbnail" href="#">
+                    <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
+                </a>
+            </div>
+            <div className="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a className="thumbnail" href="#">
+                    <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
+                </a>
+            </div>
+            <div className="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a className="thumbnail" href="#">
+                    <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
+                </a>
+            </div>
+            <div className="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a className="thumbnail" href="#">
+                    <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
+                </a>
+            </div>
           </div>
-          <div className="col-lg-3 col-md-4 col-xs-6 thumb">
-              <a className="thumbnail" href="#">
-                  <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
-              </a>
+
+          <div className="row">
+            <div className="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a className="thumbnail" href="#">
+                    <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
+                </a>
+            </div>
+            <div className="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a className="thumbnail" href="#">
+                    <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
+                </a>
+            </div>
+            <div className="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a className="thumbnail" href="#">
+                    <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
+                </a>
+            </div>
+            <div className="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a className="thumbnail" href="#">
+                    <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
+                </a>
+            </div>
           </div>
-          <div className="col-lg-3 col-md-4 col-xs-6 thumb">
-              <a className="thumbnail" href="#">
-                  <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
-              </a>
-          </div>
-          <div className="col-lg-3 col-md-4 col-xs-6 thumb">
-              <a className="thumbnail" href="#">
-                  <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
-              </a>
-          </div>
-          <div className="col-lg-3 col-md-4 col-xs-6 thumb">
-              <a className="thumbnail" href="#">
-                  <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
-              </a>
-          </div>
-          <div className="col-lg-3 col-md-4 col-xs-6 thumb">
-              <a className="thumbnail" href="#">
-                  <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
-              </a>
-          </div>
-          <div className="col-lg-3 col-md-4 col-xs-6 thumb">
-              <a className="thumbnail" href="#">
-                  <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
-              </a>
-          </div>
-          <div className="col-lg-3 col-md-4 col-xs-6 thumb">
-              <a className="thumbnail" href="#">
-                  <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
-              </a>
-          </div>
-          <div className="col-lg-3 col-md-4 col-xs-6 thumb">
-              <a className="thumbnail" href="#">
-                  <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
-              </a>
-          </div>
-          <div className="col-lg-3 col-md-4 col-xs-6 thumb">
-              <a className="thumbnail" href="#">
-                  <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
-              </a>
-          </div>
-          <div className="col-lg-3 col-md-4 col-xs-6 thumb">
-              <a className="thumbnail" href="#">
-                  <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
-              </a>
-          </div>
-          <div className="col-lg-3 col-md-4 col-xs-6 thumb">
-              <a className="thumbnail" href="#">
-                  <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
-              </a>
+
+          <div className="row">
+            <div className="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a className="thumbnail" href="#">
+                    <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
+                </a>
+            </div>
+            <div className="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a className="thumbnail" href="#">
+                    <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
+                </a>
+            </div>
+            <div className="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a className="thumbnail" href="#">
+                    <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
+                </a>
+            </div>
+            <div className="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a className="thumbnail" href="#">
+                    <img className="img-responsive" src="http://placehold.it/400x300" alt="" />
+                </a>
+            </div>
           </div>
       </div>
     );
