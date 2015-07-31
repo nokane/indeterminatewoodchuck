ef65a7de52d466c3bc8369b535226805439e4f54
Niall O'Kane <okanenm@gmail.com>
2015-07-31 16:21:00 -0700

    (feat) add icons to features

--- client/login/js/components/features.js
+++ client/login/js/components/features.js
@@ -8,67 +8,78 @@ var Features = React.createClass({
         <div className="row feat-row">
           <div className="column feat-column">
             <div className="img-icon">
-              <img className="img-responsive" src="./assets/icons/needle_icon.png" alt="" />
+              <img className="img-responsive" src="./assets/icons/video_icon.png" alt="" />
             </div>
-            <h3>Effective tool</h3>
-            <span>
-              Communicate with your customers in real time over high quality video
-              and text chat
-            </span>
+            <div className="feat-text">
+              <h3>High Quality Video</h3>
+              <span>
+                Communicate with your customers in real time over high quality video
+                and text chat
+              </span>
+            </div>
           </div>
           <div className="column feat-column">
             <div className="img-icon">
               <img className="img-responsive" src="./assets/icons/needle_icon.png" alt="" />
             </div>
-            <h3>Seamless integration</h3>
-            <span>
-              Integrate a support chat interface for your customers on your e-commerce
-              website with 5 lines of code
-            </span>
-          </div>
-        </div>
-        <div className="row feat-row">
-          <div className="column feat-column">
-            <div className="img-icon">
-              <img className="img-responsive" src="./assets/icons/needle_icon.png" alt="" />
+            <div className="feat-text">
+              <h3>Seamless Integration</h3>
+              <span>
+                Integrate a support chat interface for your customers on your e-commerce
+                website with 3 lines of code
+              </span>
             </div>
-            <h3>Easy to Use</h3>
-            <span>
-              An intuitive support staff portal that allows representatives from your
-              business to communicate with customers
-            </span>
-          </div>
-          <div className="column feat-column">
-            <div className="img-icon">
-              <img className="img-responsive" src="./assets/icons/needle_icon.png" alt="" />
-            </div>
-            <h3>Flexible Styling</h3>
-            <span>
-              Free reign to style the support chat interface to follow the styling layout
-              of your entire e-commerce business
-            </span>
           </div>
         </div>
         <div className="row feat-row">
           <div className="column feat-column">
             <div className="img-icon">
-              <img className="img-responsive" src="./assets/icons/needle_icon.png" alt="" />
+              <img className="img-responsive" src="./assets/icons/computer_icon.png" alt="" />
             </div>
-            <h3>EXAMPLE</h3>
-            <span>
-              Multiple customer chat interface mechanisms that you can use on your e-commerce
-              website
-            </span>
+            <div className="feat-text">
+              <h3>Support Staff Portal</h3>
+              <span>
+                An intuitive support staff portal that allows representatives from your
+                business to communicate with customers
+              </span>
+            </div>
           </div>
           <div className="column feat-column">
             <div className="img-icon">
-              <img className="img-responsive" src="./assets/icons/needle_icon.png" alt="" />
+              <img className="img-responsive" src="./assets/icons/brush_icon.png" alt="" />
             </div>
-            <h3>Historical support data</h3>
-            <span>
-              Access to historical information of all previous chat sessions that have
-              occurred
-            </span>
+            <div className="feat-text">
+              <h3>Flexible Styling</h3>
+              <span>
+                Free reign to style the support chat interface to follow the styling layout
+                of your entire e-commerce business
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="row feat-row">
+          <div className="column feat-column">
+            <div className="img-icon">
+              <img className="img-responsive" src="./assets/icons/customer_icon.png" alt="" />
+            </div>
+            <div className="feat-text">
+              <h3>Efficient Customer Queueing</h3>
+              <span>
+                Robust queueing system allocates your support staff resources efficiently to
+                minimize customer wait time
+              </span>
+            </div>
+          </div>
+          <div className="column feat-column">
+            <div className="img-icon">
+              <img className="img-responsive" src="./assets/icons/document_icon.png" alt="" />
+            </div>
+            <div className="feat-text">
+            <h3>Historical Support Data</h3>
+              <span>
+                Access to historical data of all previous customer support chat sessions
+              </span>
+            </div>
           </div>
         </div>
 
