b4f1cbe6ec83b70196c8875e18f8659d430fa06b
Niall O'Kane <okanenm@gmail.com>
2015-07-31 10:48:47 -0700

    (feat) add Features React component

---
+++ client/login/js/components/features.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Features = React.createClass({
+
+  render: function() {
+    return (
+      <div className='features'>
+      </div>
+    );
+  }
+});
+
+module.exports = Features;
