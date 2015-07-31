b17e76adcd4744ab3525ebee092230799497671f
Niall O'Kane <okanenm@gmail.com>
2015-07-31 16:31:28 -0700

    (feat) add intro text

--- client/login/js/components/intro.js
+++ client/login/js/components/intro.js
@@ -4,7 +4,16 @@ var Intro = React.createClass({
 
   render: function() {
     return (
-    
+      <div className='intro-text row'>
+        <span>
+          Portalize is a customer support platform that enables e-commerce businesses to
+          utilize video chat on their websites, providing a means to communicate face to face with
+          customers. With just a few lines of code from the Portalize JavaScript library, businesses
+          are able to add a customer side video chat interface to their website. In addition, 
+          Portalize provides support staff with a portal to interact with their customers. Our service makes it easy
+          for businesses to provide live customer support, improving their customers' shopping experience
+        </span>
+      </div>
     );
   }
 });
