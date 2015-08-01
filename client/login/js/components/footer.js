2b80d718d52645753a9932c3cab895c6171551dd
Leon Yip <lyip1992@gmail.com>
2015-08-01 15:39:05 -0700

    (feat) add footer scaffolding

--- client/login/js/components/footer.js
+++ client/login/js/components/footer.js
@@ -4,33 +4,51 @@ var Footer = React.createClass({
 
   render: function(){
     return (
-      <footer>
-        <div className="container">
-          <div className="row">
-            <div className="col-md-4">
-              <span className="copyright">Copyright &copy; Your Website 2014</span>
+      <footer className="text-center">
+        <div className="footer-above">
+            <div className="container">
+                <div className="row col-md-12">
+                    <div className="footer-col col-xs-4">
+                        <h3>Location</h3>
+                        <p>3481 Melrose Place<br></br>Beverly Hills, CA 90210</p>
+                    </div>
+                    <div className="footer-col col-xs-4">
+                        <h3>Around the Web</h3>
+                        <ul className="list-inline">
+                            <li>
+                                <a href="#" className="btn-social btn-outline"><i className="fa fa-fw fa-facebook"></i></a>
+                            </li>
+                            <li>
+                                <a href="#" className="btn-social btn-outline"><i className="fa fa-fw fa-google-plus"></i></a>
+                            </li>
+                            <li>
+                                <a href="#" className="btn-social btn-outline"><i className="fa fa-fw fa-twitter"></i></a>
+                            </li>
+                            <li>
+                                <a href="#" className="btn-social btn-outline"><i className="fa fa-fw fa-linkedin"></i></a>
+                            </li>
+                            <li>
+                                <a href="#" className="btn-social btn-outline"><i className="fa fa-fw fa-dribbble"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="footer-col col-xs-4">
+                        <h3>About Freelancer</h3>
+                        <p>Freelance is a free to use, open source Bootstrap theme created by <a href="http://startbootstrap.com">Start Bootstrap</a>.</p>
+                    </div>
+                </div>
             </div>
-            <div className="col-md-4">
-              <ul className="list-inline social-buttons">
-                <li><a href="#"><i className="fa fa-twitter"></i></a>
-                </li>
-                <li><a href="#"><i className="fa fa-facebook"></i></a>
-                </li>
-                <li><a href="#"><i className="fa fa-linkedin"></i></a>
-                </li>
-              </ul>
-            </div>
-            <div className="col-md-4">
-              <ul className="list-inline quicklinks">
-                <li><a href="#">Privacy Policy</a>
-                </li>
-                <li><a href="#">Terms of Use</a>
-                </li>
-              </ul>
-            </div>
-          </div>
         </div>
-      </footer>
+        <div className="footer-below">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12">
+                        Copyright &copy; Your Website 2014
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
     );
   }
 
