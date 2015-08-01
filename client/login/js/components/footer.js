c57f414fc3f72322bdfe69f1e1c5a0d9da1819c9
Leon Yip <lyip1992@gmail.com>
2015-08-01 15:59:38 -0700

    (feat) fix indentation, remove dribble link, add location

--- client/login/js/components/footer.js
+++ client/login/js/components/footer.js
@@ -6,49 +6,46 @@ var Footer = React.createClass({
     return (
       <footer className="text-center">
         <div className="footer-above">
-            <div className="container">
-                <div className="row col-md-12">
-                    <div className="footer-col col-xs-4">
-                        <h3>Location</h3>
-                        <p>3481 Melrose Place<br></br>Beverly Hills, CA 90210</p>
-                    </div>
-                    <div className="footer-col col-xs-4">
-                        <h3>Around the Web</h3>
-                        <ul className="list-inline">
-                            <li>
-                                <a href="#" className="btn-social btn-outline"><i className="fa fa-fw fa-facebook"></i></a>
-                            </li>
-                            <li>
-                                <a href="#" className="btn-social btn-outline"><i className="fa fa-fw fa-google-plus"></i></a>
-                            </li>
-                            <li>
-                                <a href="#" className="btn-social btn-outline"><i className="fa fa-fw fa-twitter"></i></a>
-                            </li>
-                            <li>
-                                <a href="#" className="btn-social btn-outline"><i className="fa fa-fw fa-linkedin"></i></a>
-                            </li>
-                            <li>
-                                <a href="#" className="btn-social btn-outline"><i className="fa fa-fw fa-dribbble"></i></a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="footer-col col-xs-4">
-                        <h3>About Freelancer</h3>
-                        <p>Freelance is a free to use, open source Bootstrap theme created by <a href="http://startbootstrap.com">Start Bootstrap</a>.</p>
-                    </div>
-                </div>
+          <div className="container">
+            <div className="row col-md-12">
+              <div className="footer-col col-xs-4">
+                <h3>Location</h3>
+                <p>944 Market St.<br></br>San Francisco, CA 94102</p>
+              </div>
+              <div className="footer-col col-xs-4">
+                <h3>Around the Web</h3>
+                <ul className="list-inline">
+                  <li>
+                    <a href="#" className="btn-social btn-outline"><i className="fa fa-fw fa-facebook"></i></a>
+                  </li>
+                  <li>
+                    <a href="#" className="btn-social btn-outline"><i className="fa fa-fw fa-google-plus"></i></a>
+                  </li>
+                  <li>
+                    <a href="#" className="btn-social btn-outline"><i className="fa fa-fw fa-twitter"></i></a>
+                  </li>
+                  <li>
+                    <a href="#" className="btn-social btn-outline"><i className="fa fa-fw fa-linkedin"></i></a>
+                  </li>
+                </ul>
+              </div>
+              <div className="footer-col col-xs-4">
+                <h3>About Freelancer</h3>
+                <p>Freelance is a free to use, open source Bootstrap theme created by <a href="http://startbootstrap.com">Start Bootstrap</a>.</p>
+              </div>
             </div>
+          </div>
         </div>
         <div className="footer-below">
-            <div className="container">
-                <div className="row col-md-12">
-                    <div className="col-xs-12">
-                        Copyright &copy; Portalize 2015
-                    </div>
-                </div>
+          <div className="container">
+            <div className="row col-md-12">
+              <div className="col-xs-12">
+                Copyright &copy; Portalize 2015
+              </div>
             </div>
+          </div>
         </div>
-    </footer>
+      </footer>
     );
   }
 
