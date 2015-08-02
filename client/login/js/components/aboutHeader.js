6c2836939016c8040a0d6a59f0f88a27c4e86493
Niall O'Kane <okanenm@gmail.com>
2015-08-01 18:39:35 -0700

    (feat) add title to AboutHeader

--- client/login/js/components/aboutHeader.js
+++ client/login/js/components/aboutHeader.js
@@ -5,9 +5,22 @@ var AboutHeader = React.createClass({
   render: function() {
     return (
       <div className='about-header'>
-
-        <img className='portal-img' src='./assets/about/portal_side.png' alt='' />
-        <img className='customer-img' src='./assets/about/customer_side.png' alt='' />
+        <div className='header-text'>
+          <div className='portalize-title'>
+            <span>PORTALIZE</span>
+          </div>
+          <div className='title-caption'>
+            <span>THE DIRECT LINK BETWEEN YOUR SUPPORT STAFF AND CUSTOMERS</span>
+          </div>
+        </div>
+        <div className='about-images'>
+          <div className='div-portal-img'>
+            <img className='portal-img' src='./assets/about/portal_side.png' alt='' />
+          </div>
+          <div className='div-customer-img'>
+            <img className='customer-img' src='./assets/about/customer_side.png' alt='' />
+          </div>
+        </div>
       </div>
     );
   }
