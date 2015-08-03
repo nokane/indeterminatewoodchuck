ef947e73b8d35ec7175c1250fd4ac223cf6f446a
Niall O'Kane <okanenm@gmail.com>
2015-08-03 10:47:24 -0700

    (refactor) refactor captions on about page

--- client/login/js/components/aboutHeader.js
+++ client/login/js/components/aboutHeader.js
@@ -10,15 +10,15 @@ var AboutHeader = React.createClass({
             <span>PORTALIZE</span>
           </div>
           <div className='title-caption'>
-            <span>ADD VIDEO CHAT SUPPORT TO YOUR E-COMMERCE WEBSITE</span>
+            <span>LIVE CUSTOMER SUPPORT FOR YOUR E-COMMERCE WEBSITE</span>
           </div>
         </div>
         <div className='about-images'>
-        <span className='image-caption'>CONNECT YOUR SUPPORT STAFF</span>
+        <span className='image-caption'>Connect your support staff ...</span>
           <div className='div-portal-img'>
             <img className='portal-img' src='./assets/about/portal_side.png' alt='' />
           </div>
-          <span className='image-caption'>TO YOUR CUSTOMERS WITH 3 LINES OF CODE</span>
+          <span className='image-caption'>with your customers</span>
           <div className='div-customer-img'>
             <img className='customer-img' src='./assets/about/customer_side.png' alt='' />
           </div>
